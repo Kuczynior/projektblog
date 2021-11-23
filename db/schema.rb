463372f0f8678f279c09982f1d16ab77492e89c1
Kuczynior <82318577+Kuczynior@users.noreply.github.com>
2021-11-23 21:00:37 +0100

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_175954) do
+ActiveRecord::Schema.define(version: 2021_11_23_195751) do
 
   create_table "catergories", force: :cascade do |t|
     t.string "name"
